94a602f74f8aa2984d7698663ac32d29e64d8fc6
eswarrodda <141038986+eswarrodda@users.noreply.github.com>
2024-10-27 19:13:31 +0530

    Add files via upload
    iteration_program

---
+++ Printingpattern3.java
@@ -0,0 +1,18 @@
+package printingpattern3;
+
+public class Printingpattern3 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				if(i==j || j==6-i ) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}System.out.println("");
+		}
+	}
+
+}
